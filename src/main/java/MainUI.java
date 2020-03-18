e3b281fd35b2d7d2122d3c4516ea4287bf883b29
Markus Petritz <petritzdesigns@gmail.com>
2020-03-18 09:29:41 +0100

    00 Init java swing project with maven for dependencies [PM]

---
+++ src/main/java/MainUI.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MainUI {
+    private JPanel contentPane;
+    private JButton clickMeButton;
+
+    public MainUI() {
+        clickMeButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null, "Hello World");
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("MainUI");
+        frame.setContentPane(new MainUI().contentPane);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
