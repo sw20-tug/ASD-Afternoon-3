a37868a5790a23752eca7363470a6a35709082f3
Markus Petritz <petritz@student.tugraz.at>
2020-03-18 11:51:41 +0100

    FLAT-000 Added sample testcase [PM,FC]

---
+++ src/main/java/Sample.java
@@ -0,0 +1,16 @@
+public class Sample {
+
+    private String test;
+
+    public Sample(String test) {
+        this.test = test;
+    }
+
+    public String getTest() {
+        return test;
+    }
+
+    public int doStuff() {
+        return test.length();
+    }
+}
---
+++ src/test/java/SampleTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SampleTest {
+
+    @Test
+    public void doStuff() {
+        Sample sample = new Sample("1234");
+        assertEquals(sample.doStuff(), 4);
+    }
+}
