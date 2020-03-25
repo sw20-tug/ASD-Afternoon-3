b48139a9c5c7e65ddab7d425dc0bbf7ebde802ff
Sami Chemloul <chemloul@student.tugraz.at>
2020-03-25 11:05:14 +0100

    FLAT-001C Added functionality to button [SC,CF]

--- src/main/java/at/tugraz/asdafternoon3/ui/MainUI.java
+++ src/main/java/at/tugraz/asdafternoon3/ui/MainUI.java
@@ -1,17 +1,36 @@
 package at.tugraz.asdafternoon3.ui;
 
+import at.tugraz.asdafternoon3.data.Flat;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class MainUI {
     private JPanel contentPane;
+    private JPanel Panel1;
+    private JButton btCreateNewFlat;
+    private JTextField tfName;
+    private JTextField tfAddress;
+    private JTextField tfSize;
     private JButton clickMeButton;
 
     public MainUI() {
-        clickMeButton.addActionListener(new ActionListener() {
+
+        btCreateNewFlat.addActionListener(new ActionListener() {
+            @Override
             public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(null, "Hello World");
+                Flat newFlat;
+                int size = 0;
+
+                try {
+                    size = Integer.parseInt(tfSize.getText());
+                }
+                catch (Exception ex)
+                {
+                    System.out.println("Exception: Couldn't convert string to int: " + ex.getLocalizedMessage());
+                }
+                newFlat = new Flat(tfName.getText(), size, tfAddress.getText());
             }
         });
     }
@@ -22,5 +41,9 @@ public class MainUI {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
     }
 }
