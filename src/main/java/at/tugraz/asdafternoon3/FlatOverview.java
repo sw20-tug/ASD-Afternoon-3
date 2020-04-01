ea44a05e26d203f451fe877bf7192211e7c8f936
Jan-Heliodor Tscherko <j.tscherko@student.tugraz.at>
2020-04-01 10:14:39 +0200

    FLAT-002B Changes in FlatOverview GUI [CM,TJ]

--- src/main/java/at/tugraz/asdafternoon3/FlatOverview.java
+++ src/main/java/at/tugraz/asdafternoon3/FlatOverview.java
@@ -1,10 +1,13 @@
 package at.tugraz.asdafternoon3;
 
+import at.tugraz.asdafternoon3.ui.MainUI;
+import at.tugraz.asdafternoon3.data.Flat;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class FlatOverview {
+public class FlatOverview
+{
     private JPanel panel1;
     private JPanel main;
     private JPanel BasicOverview;
@@ -14,17 +17,30 @@ public class FlatOverview {
     private JButton cleaningScheduleButton;
     private JButton financeFurnitureAndEquipmentButton;
     private JButton financeFlatButton;
+    private JTextArea taAddress;
+    private JTextArea taSize;
+    private JTextArea taName;
 
-    public FlatOverview() {
-        button1.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-            }
-        });
+    public FlatOverview()
+    {
     }
 
-    private void createUIComponents() {
+    private void createUIComponents()
+    {
+        //Flat flat = new Flat("TestHome", 100, "Rechbauerstraße 11");
         // TODO: place custom component creation code here
+        //taName.setText(flat.getName());
+        //taSize.setText(String.format("%d",flat.getSize()));
+       // taAddress.setText(flat.getAddress());
+    }
+
+    public static void main(String[] args)
+    {
+        JFrame frame = new JFrame("FlatOverview");
+
+        frame.setContentPane(new FlatOverview().panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
     }
 }
